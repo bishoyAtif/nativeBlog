cbd9bc4070dd4fca75c5b5151894c3f399c1326b
Bishoy Atif <bishooy.atif@gmail.com>
2018-03-27 23:05:26 +0200

    Add autoloading

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,11 +1,7 @@
 <?php
 
-require_once "core/App.php";
-require_once "core/Router.php";
-require_once "core/Request.php";
-require_once "core/DB.php";
-require_once "core/Model.php";
-require_once "app/models/Post.php";
+require_once 'vendor/autoload.php';
+
 App::bind('router', new Router);
 App::bind('database', DB::connect(config('db')));
 $router = App::get('router');
@@ -14,7 +10,6 @@ require_once "routes.php";
 $routeData = $router->resolveUrl();
 $controllerName = $routeData['controller'];
 $methodName = $routeData['method'];
-require_once "app/controllers/" . $controllerName . ".php";
 $controller = new $controllerName;
 $controller->$methodName(Request::get('id'));
 
